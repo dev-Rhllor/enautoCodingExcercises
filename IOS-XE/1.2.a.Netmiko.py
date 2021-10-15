c46ccb55df9fb702d79096f34618833aacbf2a4b
dev-Rhllor <dev-rhllor@protonmail.com>
2021-10-15 14:49:27 +0200

    changing the python script to main function

--- IOS-XE/1.2.a.Netmiko.py
+++ IOS-XE/1.2.a.Netmiko.py
@@ -1,15 +1,19 @@
 from netmiko import ConnectHandler
 import pandas as pd
 
-sandboxRouter= {
-    'device_type': 'cisco_ios',
-    'host':   'sandbox-iosxe-recomm-1.cisco.com',
-    'username': 'developer',
-    'password': 'C1sco12345',
-    'port' : 22
-}
+def main():
+    sandboxRouter= {
+        'device_type': 'cisco_ios',
+        'host':   'sandbox-iosxe-recomm-1.cisco.com',
+        'username': 'developer',
+        'password': 'C1sco12345',
+        'port' : 22
+    }
 
-sandboxConnection = ConnectHandler(**sandboxRouter)
-sandboxIP = sandboxConnection.send_command("show ip interface brief",use_textfsm=True)
-sandboxIPdf=pd.DataFrame(sandboxIP)
-print(sandboxIPdf)
+    sandboxConnection = ConnectHandler(**sandboxRouter)
+    sandboxIP = sandboxConnection.send_command("show ip interface brief",use_textfsm=True)
+    sandboxIPdf=pd.DataFrame(sandboxIP)
+    print(sandboxIPdf)
+
+if __name__ == '__main__':
+    main()
